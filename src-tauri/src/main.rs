68873c8720980e805f9a3f142573ed8a7fff8537
Rosa Pham <tpham2@blueorigin.com>
2021-08-18 13:38:49 -0700

    Works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,7 +13,8 @@ use std::fs::File;
 use std::sync::Arc;
 use arrow::json::writer::record_batches_to_json_rows;
 use serde_json::to_string;
-use tauri::async_runtime::{channel, spawn};
+use settimeout::set_timeout;
+use std::time::Duration;
 
 #[tauri::command]
 async fn my_custom_command(window: tauri::Window) {
@@ -21,31 +22,48 @@ async fn my_custom_command(window: tauri::Window) {
     Field::new("exp_time", DataType::Utf8, false),
     Field::new("utc_time", DataType::Utf8, false),
     Field::new("lat_long_1", DataType::Utf8, false),
+    
+    Field::new("lat_long_2", DataType::Utf8, false),
+    Field::new("pos_1", DataType::Utf8, false),
+    Field::new("pos_2", DataType::Utf8, false),
+    Field::new("pos_3", DataType::Utf8, false),
+    Field::new("gps_alt", DataType::Utf8, false),
+    Field::new("vel_1", DataType::Utf8, false),
+    Field::new("vel_2", DataType::Utf8, false),
+    Field::new("vel_3", DataType::Utf8, false),
+    Field::new("accel_1", DataType::Utf8, false),
+    Field::new("accel_2", DataType::Utf8, false),
+    Field::new("accel_3", DataType::Utf8, false),
+    Field::new("mag_accel", DataType::Utf8, false),
+    Field::new("att_1", DataType::Utf8, false),
+    Field::new("att_2", DataType::Utf8, false),
+    Field::new("att_3", DataType::Utf8, false),
+    Field::new("ang_vel_1", DataType::Utf8, false),
+    Field::new("ang_vel_2", DataType::Utf8, false),
+    Field::new("ang_vel_3", DataType::Utf8, false),
+    Field::new("warnings_liftoff_warn", DataType::Utf8, false),
+    Field::new("warnings_rcs_warn", DataType::Utf8, false),
+    Field::new("warnings_drogue_chute_warn", DataType::Utf8, false),
+    Field::new("warnings_landing_warn", DataType::Utf8, false),
+    Field::new("warnings_chute_fault_warn", DataType::Utf8, false)
   ]);
 
   let file = File::open("src/data.csv").unwrap();
 
   let mut csv = csv::Reader::new(file, Arc::new(schema), true, None, 1, None, None);
 
-  let (tx, mut rx) = channel(1);
-  spawn(async move {
     while let Some(m) = csv.next() {
       let n = m.unwrap();
-      if let Err(_) = tx.send(n).await {
-        println!("receiver dropped");
-        return;
-      }
-    }
-  });
-
-  while let Some(i) = rx.recv().await {
-    let json_rows = record_batches_to_json_rows(&[i]);
+    let json_rows = record_batches_to_json_rows(&[n]);
     let serialized = to_string(&json_rows).unwrap();
     
+    // Delay process so frontend not freaking out
+    set_timeout(Duration::from_micros(1_000)).await;
+
     window
       .emit("rust-event", serialized)
       .expect("failed to emit");
-  }
+}
 }
 
 fn main() {
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useCallback, useState, useMemo } from 'react';
 import { invoke } from '@tauri-apps/api/tauri'
 
 const App = () => {
-  const [altitude, setAltitude] = useState(0);
+  const [value, setValue] = useState(null);
 
   useEffect(() => {
     listen('rust-event', myCallback)
@@ -12,9 +12,7 @@ const App = () => {
 
   const myCallback = useCallback((e) => {
     const batch = JSON.parse(e.payload);
-    const num = batch[0].exp_time;
-    console.log(e);
-    setAltitude(num);
+    setValue(batch[0])
   })
 
   const handleClick = async () => {
@@ -25,7 +23,36 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <button onClick={handleClick}>Click Me!</button>
-        <span>{altitude}</span>
+        {value &&
+        <>
+        <span>Exp Time: {value.exp_time}</span>
+        <span>Utc Time: {value.utc_time}</span>
+        <span>Lat Long 1: {value.lat_long_1}</span>
+        <span>Lat Long 2:{value.lat_long_2}</span>
+        <span>Pos 1: {value.pos_1}</span>
+        <span>Pos 2: {value.pos_2}</span>
+        <span>Pos 3: {value.pos_3}</span>
+        <span>Gps alt: {value.gps_alt}</span>
+        <span>Vel 1: {value.vel_1}</span>
+        <span>Vel 2: {value.vel_2}</span>
+        <span>Vel 3:{value.vel_3}</span>
+        <span>Accel 1: {value.accel_1}</span>
+        <span>Accel 2: {value.accel_2}</span>
+        <span>Accel 3: {value.accel_3}</span>
+        <span>Mag Accel: {value.mag_accel}</span>
+        <span>Att 1: {value.att_1}</span>
+        <span>Att 2: {value.att_2}</span>
+        <span>Att 3: {value.att_3}</span>
+        <span>Ang vel: {value.ang_vel_1}</span>
+        <span>Ang vel: {value.ang_vel_2}</span>
+        <span>Ang vel: {value.ang_vel_3}</span>
+        <span>Warning Liff Off: {value.warnings_liftoff_warn}</span>
+        <span>Warning Rcs: {value.warnings_rcs_warn}</span>
+        <span>Warning Drogue: {value.warnings_drogue_chute_warn}</span>
+        <span>Warning Landing: {value.warnings_landing_warn}</span>
+        <span>Warning Chute: {value.warnings_chute_fault_warn}</span>
+        </>
+        }
       </header>
     </div>
   );
