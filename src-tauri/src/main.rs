9f3238cd5abd2cd1ddcfed03ba4d144361b98740
Rosa Pham <tpham2@blueorigin.com>
2021-12-27 11:53:33 -0800

    Freeze when loading truth data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,13 +9,15 @@
 
 use tauri::Manager;
 use arrow::csv;
-use arrow::datatypes::{DataType, Field, Schema};
+use arrow::datatypes::{DataType, Field, Schema, TimeUnit, Time64NanosecondType};
+use arrow::array::{StringArray};
 use std::fs::File;
 use std::sync::Arc;
 use arrow::json::writer::record_batches_to_json_rows;
 use serde_json::to_string;
 use settimeout::set_timeout;
 use std::time::Duration;
+use arrow::record_batch::RecordBatch;
 
 #[tauri::command]
 fn close_splashscreen(window: tauri::Window) {
@@ -31,41 +33,41 @@ fn close_splashscreen(window: tauri::Window) {
 async fn my_custom_command(window: tauri::Window) {
   let truth_schema = Schema::new(vec![
     Field::new("TIME_NANOSECONDS_TAI", DataType::Utf8, false),
-    Field::new("truth_pos_CON_ECEF_ECEF_M[1]", DataType::Utf8, false),
-    Field::new("truth_pos_CON_ECEF_ECEF_M[2]", DataType::Utf8, false),
+    Field::new("truth_pos_CON_ECEF_ECEF_M[1]", DataType::Float64, false),
+    Field::new("truth_pos_CON_ECEF_ECEF_M[2]", DataType::Float64, false),
     
-    Field::new("truth_pos_CON_ECEF_ECEF_M[3]", DataType::Utf8, false),
-    Field::new("truth_vel_CON_ECEF_ECEF_MpS[1]", DataType::Utf8, false),
-    Field::new("truth_vel_CON_ECEF_ECEF_MpS[2]", DataType::Utf8, false),
-    Field::new("truth_vel_CON_ECEF_ECEF_MpS[3]", DataType::Utf8, false),
-    Field::new("truth_quat_CON2ECEF[1]", DataType::Utf8, false),
-    Field::new("truth_quat_CON2ECEF[2]", DataType::Utf8, false),
-    Field::new("truth_quat_CON2ECEF[3]", DataType::Utf8, false),
-    Field::new("truth_quat_CON2ECEF[4]", DataType::Utf8, false)
+    Field::new("truth_pos_CON_ECEF_ECEF_M[3]", DataType::Float64, false),
+    Field::new("truth_vel_CON_ECEF_ECEF_MpS[1]", DataType::Float64, false),
+    Field::new("truth_vel_CON_ECEF_ECEF_MpS[2]", DataType::Float64, false),
+    Field::new("truth_vel_CON_ECEF_ECEF_MpS[3]", DataType::Float64, false),
+    Field::new("truth_quat_CON2ECEF[1]", DataType::Float64, false),
+    Field::new("truth_quat_CON2ECEF[2]", DataType::Float64, false),
+    Field::new("truth_quat_CON2ECEF[3]", DataType::Float64, false),
+    Field::new("truth_quat_CON2ECEF[4]", DataType::Float64, false)
   ]);
 
   let dlc_schema = Schema::new(vec![
     Field::new("TIME_NANOSECONDS_TAI", DataType::Utf8, false),
-    Field::new("DATA_DELTA_VEL[1]", DataType::Utf8, false),
-    Field::new("DATA_DELTA_VEL[2]", DataType::Utf8, false),
+    Field::new("DATA_DELTA_VEL[1]", DataType::Float64, false),
+    Field::new("DATA_DELTA_VEL[2]", DataType::Float64, false),
     
-    Field::new("DATA_DELTA_VEL[3]", DataType::Utf8, false),
-    Field::new("DATA_DELTA_ANGLE[1]", DataType::Utf8, false),
-    Field::new("DATA_DELTA_ANGLE[2]", DataType::Utf8, false),
-    Field::new("DATA_DELTA_ANGLE[3]", DataType::Utf8, false)
+    Field::new("DATA_DELTA_VEL[3]", DataType::Float64, false),
+    Field::new("DATA_DELTA_ANGLE[1]", DataType::Float64, false),
+    Field::new("DATA_DELTA_ANGLE[2]", DataType::Float64, false),
+    Field::new("DATA_DELTA_ANGLE[3]", DataType::Float64, false)
   ]);
 
   let lidar_schema = Schema::new(vec![
     Field::new("TIME_NANOSECONDS_TAI", DataType::Utf8, false),
-    Field::new("OMPS_Range_M[1]", DataType::Utf8, false),
-    Field::new("OMPS_Range_M[2]", DataType::Utf8, false),
+    Field::new("OMPS_Range_M[1]", DataType::Float64, false),
+    Field::new("OMPS_Range_M[2]", DataType::Float64, false),
     
-    Field::new("OMPS_Range_M[3]", DataType::Utf8, false),
-    Field::new("OMPS_Range_M[4]", DataType::Utf8, false),
-    Field::new("OMPS_DopplerSpeed_MpS[1]", DataType::Utf8, false),
-    Field::new("OMPS_DopplerSpeed_MpS[2]", DataType::Utf8, false),
-    Field::new("OMPS_DopplerSpeed_MpS[3]", DataType::Utf8, false),
-    Field::new("OMPS_DopplerSpeed_MpS[4]", DataType::Utf8, false),
+    Field::new("OMPS_Range_M[3]", DataType::Float64, false),
+    Field::new("OMPS_Range_M[4]", DataType::Float64, false),
+    Field::new("OMPS_DopplerSpeed_MpS[1]", DataType::Float64, false),
+    Field::new("OMPS_DopplerSpeed_MpS[2]", DataType::Float64, false),
+    Field::new("OMPS_DopplerSpeed_MpS[3]", DataType::Float64, false),
+    Field::new("OMPS_DopplerSpeed_MpS[4]", DataType::Float64, false),
   ]);
 
   // Open file
@@ -74,23 +76,77 @@ async fn my_custom_command(window: tauri::Window) {
   let lidar_file = File::open("data/Flight1_Catered_Dataset/Data/commercial_lidar.csv").unwrap();
 
   // Get csv Reader using schema
-  let mut truth_csv = csv::Reader::new(truth_file, Arc::new(truth_schema), true, None, 1, None, None);
-  let mut dlc_csv = csv::Reader::new(dlc_file, Arc::new(dlc_schema), true, None, 1, None, None);
-  let mut lidar_csv = csv::Reader::new(lidar_file, Arc::new(lidar_schema), true, None, 1, None, None);
+  let mut truth_csv = csv::Reader::new(truth_file, Arc::new(truth_schema.clone()), true, None, 1, None, None);
+  let mut dlc_csv = csv::Reader::new(dlc_file, Arc::new(dlc_schema.clone()), true, None, 1, None, None);
+  let mut lidar_csv = csv::Reader::new(lidar_file, Arc::new(lidar_schema.clone()), true, None, 1, None, None);
 
+  let mut start: i64 = 1602596010219040000;
+  let end: i64 = 1602596810212950000;
 
-  while let Some(m) = truth_csv.next() {
-    let n = m.unwrap();
-    let json_rows = record_batches_to_json_rows(&[n]);
-    let serialized = to_string(&json_rows).unwrap();
+  let mut truth_record_batch: RecordBatch = RecordBatch::new_empty(Arc::new(truth_schema));
+  // let mut lidar_record_batch: RecordBatch = RecordBatch::new_empty(Arc::new(lidar_schema));
+
+  // let mut lidar_should_be_next:bool = true ;
+  // let mut lidar_time:i64 = 0;
+  
+  while start <= end {
+    println!("{}", &start);
+    // set_timeout(Duration::from_micros(10_000)).await;
+    // window
+    //     .emit("tai-event", &start)
+    //     .expect("failed to emit");
+
+    truth_record_batch = truth_csv.nth(0).unwrap().unwrap();
+    let truth_time = get_time(&truth_record_batch);
     
-    // Delay process so frontend not freaking out
-    set_timeout(Duration::from_micros(1_000)).await;
-    window
-      .emit("rust-event", serialized)
-      .expect("failed to emit");
-  }
+    // if lidar_should_be_next {
+    //   lidar_record_batch = lidar_csv.nth(0).unwrap().unwrap();
+    //   lidar_time = get_time(&lidar_record_batch);
+    // }
 
+    if truth_time - start <= 1000 {
+      let json_rows1 = record_batches_to_json_rows(&[truth_record_batch.clone()]);
+      let serialized1 = to_string(&json_rows1).unwrap();
+
+        // Delay process so frontend not freaking out
+        set_timeout(Duration::from_micros(10000)).await;
+      window
+        .emit("truth-event", serialized1)
+        .expect("failed to emit");
+    } 
+
+    // if lidar_time - start <= 10000 {
+    //   let json_rows2 = record_batches_to_json_rows(&[lidar_record_batch.clone()]);
+    //   let serialized2 = to_string(&json_rows2).unwrap();
+
+    //     // Delay process so frontend not freaking out
+    //     // set_timeout(Duration::from_micros(10_000)).await;
+    //   window
+    //     .emit("lidar-event", serialized2)
+    //     .expect("failed to emit");
+    // } else {
+    //   // keep current record batch
+    //   lidar_should_be_next = false;
+    // } 
+
+    start = start + 10000000; // 10_000_000
+  }
+    
+}
+
+fn get_time(record_batch: &RecordBatch) -> i64 {
+  let base: f64 = 10.0;
+  let t = base.powf(18.0);
+
+  let v = record_batch
+    .column(0)
+    .as_any().downcast_ref::<StringArray>().unwrap()
+    .value(0)
+    .replace("e+18", "")
+    .parse::<f64>().unwrap();
+
+    let res = (t * v).round() as i64;
+    return res;
 }
 
 fn main() {
@@ -99,3 +155,16 @@ fn main() {
     .run(tauri::generate_context!())
     .expect("failed to run app");
 }
+
+// truth: s-1.60259601021904e+18 e-1.60259681021295e+18
+// dlc  : s-1.6025960102293e+18  e-1.602596810229e+18
+// lidar: s-1.60259621009107e+18 e-1.60259665919107e+18
+
+// t-0  :   1.60259621021e+18
+// let x, y, z = 0;
+// loop i from 1.60259601021904e+18 to 1.60259681021295e+18
+// if i === truth[x].tai: emit  and x++
+// if i === dlc[x].tai: emit  and y++
+// if i === lidar[x].tai: emit  and z++
+
+// 1.60 25 96 01 02 39 04 00 00
--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { invoke } from '@tauri-apps/api/tauri'
 
 const App = () => {
   const [isInitialLoad, setIsInitialLoad] = useState(true);
-  const [value, setValue] = useState(null);
+  const [tai, setTai] = useState(null);
+  const [truth, setTruth] = useState(null);
+  const [lidar, setLidar] = useState(null);
 
   useEffect(() => {
     if (isInitialLoad) {
-      setTimeout(() => { closeSplashScreen() }, 4000);
-      listen('rust-event', myCallback);
+      setTimeout(() => { closeSplashScreen() }, 5000);
+      // listen('tai-event', taiCb);
+      listen('truth-event', truthCb);
+      // listen('lidar-event', lidarCb);
       setIsInitialLoad(false);
     }
   }, [isInitialLoad])
@@ -19,10 +23,20 @@ const App = () => {
     return await invoke('close_splashscreen');
   }, [])
 
-  const myCallback = useCallback((e) => {
+  // const taiCb = useCallback((e) => {
+  //   const batch = JSON.parse(e.payload);
+  //   setTai(batch)
+  // })
+
+  const truthCb = useCallback((e) => {
     const batch = JSON.parse(e.payload);
-    setValue(batch[0])
+    setTruth(batch[0])
   })
+
+  // const lidarCb = useCallback((e) => {
+  //   const batch = JSON.parse(e.payload);
+  //   setLidar(batch[0])
+  // })
 
   const handleClick = async () => {
     invoke('my_custom_command').catch(error => console.log("Erorrrrr:", error));
@@ -32,49 +46,84 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <button onClick={handleClick}>Click Me!</button>
-        {value && <table>
-            <tr>
-              <td>Time</td>
-              <td>{value.TIME_NANOSECONDS_TAI}</td>
-            </tr>
+        {tai && <div>
+          <span>Time</span>
+          <span>{tai}</span>
+        </div>}
+        {truth && <table>
             <tr>
               <td>truth_pos_CON_ECEF_ECEF_M[1]</td>
-              <td>{value['truth_pos_CON_ECEF_ECEF_M[1]']}</td>
+              <td>{truth['truth_pos_CON_ECEF_ECEF_M[1]']}</td>
             </tr>
             <tr>
               <td>truth_pos_CON_ECEF_ECEF_M[2]</td>
-              <td>{value['truth_pos_CON_ECEF_ECEF_M[2]']}</td>
+              <td>{truth['truth_pos_CON_ECEF_ECEF_M[2]']}</td>
             </tr>
             <tr>
               <td>truth_pos_CON_ECEF_ECEF_M[3]</td>
-              <td>{value['truth_pos_CON_ECEF_ECEF_M[3]']}</td>
+              <td>{truth['truth_pos_CON_ECEF_ECEF_M[3]']}</td>
             </tr>
             <tr>
               <td>truth_vel_CON_ECEF_ECEF_MpS[1]</td>
-              <td>{value['truth_vel_CON_ECEF_ECEF_MpS[1]']}</td>
+              <td>{truth['truth_vel_CON_ECEF_ECEF_MpS[1]']}</td>
             </tr>
             <tr>
               <td>truth_vel_CON_ECEF_ECEF_MpS[2]</td>
-              <td> {value['truth_vel_CON_ECEF_ECEF_MpS[2]']}</td>
+              <td> {truth['truth_vel_CON_ECEF_ECEF_MpS[2]']}</td>
             </tr>
             <tr>
               <td>truth_vel_CON_ECEF_ECEF_MpS[3]</td>
-              <td> {value['truth_vel_CON_ECEF_ECEF_MpS[3]']}</td>
+              <td> {truth['truth_vel_CON_ECEF_ECEF_MpS[3]']}</td>
             </tr>
             <tr>
               <td>truth_quat_CON2ECEF[1]</td>
-              <td> {value['truth_quat_CON2ECEF[1]']}</td>
+              <td> {truth['truth_quat_CON2ECEF[1]']}</td>
             </tr>
             <tr>
               <td>truth_quat_CON2ECEF[2]</td>
-              <td> {value['truth_quat_CON2ECEF[2]']}</td>
+              <td> {truth['truth_quat_CON2ECEF[2]']}</td>
             </tr>
             <tr>
               <td>truth_quat_CON2ECEF[3]</td>
-              <td> {value['truth_quat_CON2ECEF[3]']}</td></tr>
+              <td> {truth['truth_quat_CON2ECEF[3]']}</td></tr>
             <tr>
               <td>truth_quat_CON2ECEF[4]</td>
-              <td> {value['truth_quat_CON2ECEF[4]']}</td>
+              <td> {truth['truth_quat_CON2ECEF[4]']}</td>
+            </tr>
+          </table>
+        }
+        {lidar && <table>
+            <tr>
+              <td>OMPS_Range_M[1]</td>
+              <td> {truth['OMPS_Range_M[1]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_Range_M[2]</td>
+              <td> {truth['OMPS_Range_M[2]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_Range_M[3]</td>
+              <td> {truth['OMPS_Range_M[3]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_Range_M[4]</td>
+              <td> {truth['OMPS_Range_M[4]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_DopplerSpeed_MpS[1]</td>
+              <td> {truth['OMPS_DopplerSpeed_MpS[1]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_DopplerSpeed_MpS[2]</td>
+              <td> {truth['OMPS_DopplerSpeed_MpS[2]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_DopplerSpeed_MpS[3]</td>
+              <td> {truth['OMPS_DopplerSpeed_MpS[3]']}</td>
+            </tr>
+            <tr>
+              <td>OMPS_DopplerSpeed_MpS[4]</td>
+              <td> {truth['OMPS_DopplerSpeed_MpS[4]']}</td>
             </tr>
           </table>
         }
